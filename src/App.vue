d1ec724e33e83113834a7dd86dcd20c4a7f0cac3
Camilledelmon <camille.delmon@gmail.com>
2024-07-10 09:17:57 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<div id="mymenu" className="sidnav">
+<div><img id="navlogo"  src="/src/images/logo.png" alt=""></img></div>
+<div className="sidnav_seprator" ><hr/></div>
+<div>
+    <p className="sidnav_title"> <a href="/">Accueil </a></p>
+    <p className="sidnav_title">L&#39association</p>
+    <p  className="sidnav_section" ><a href="game">presentation</a></p>
+</div>
+</div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
